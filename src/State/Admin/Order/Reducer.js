810cebc765d50383b4628d70846b45721a3f2156
Ahmed-Hany-saad <ahhasa842@gmail.com>
2024-05-05 20:48:16 +0300

    NEW

---
+++ src/State/Admin/Order/Reducer.js
@@ -0,0 +1,98 @@
+import * as actionTypes from "./ActionType";
+
+const initialState = {
+    isLoading: false,
+    orders: [],
+    error:"",
+}
+const adminOrderReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.GET_ORDERS_REQUEST:
+            return { ...state, loading: true };
+        case actionTypes.GET_ORDERS_SUCCESS:
+            return { ...state, loading: false, error: "", };
+        case actionTypes.GET_ORDERS_FAILURE:
+            return { ...state, loading: false, error: action.payload, order: [] }
+        case actionTypes.CONFIRMED_ORDERS_REQUEST:
+        case actionTypes.PLACED_ORDERS_REQUEST:
+        case actionTypes.DELIVERD_ORDERS_REQUEST:
+        case actionTypes.CANCELED_ORDERS_REQUEST:
+            return {
+                ...state,
+                isLoading: true,
+            };
+        case actionTypes.CONFIRMED_ORDERS_SUCCESS:
+            return {
+                ...state,
+                confirmed: action.payload,
+                isLoading: false,
+            };
+        case actionTypes.PLACED_ORDERS_SUCCESS:
+            return {
+                ...state,
+                placed: action.payload,
+                isLoading: false,
+            };
+        case actionTypes.DELIVERD_ORDERS_SUCCESS:
+            return {
+                ...state,
+                delivered: action.payload,
+                isLoading: false,
+            };
+        case actionTypes.CANCELED_ORDERS_SUCCESS:
+            return {
+                delivered: action.payload,
+                isLoading: false,
+            };
+        case actionTypes.CONFIRMED_ORDERS_FAILURE:
+        case actionTypes.PLACED_ORDERS_FAILURE:
+        case actionTypes.DELIVERD_ORDERS_FAILURE:
+        case actionTypes.CANCELED_ORDERS_FAILURE:
+            return {
+                ...state,
+                error: action.payload,
+                isLoading: false,
+            };
+        case actionTypes.DELETE_ORDERS_REQUEST:
+            return { ...state, loading: true };
+        case actionTypes.DELETE_ORDERS_REQUEST:
+            return {
+                ...state, loading: true
+            }
+        case actionTypes.DELETE_ORDERS_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                orders: state.orders.filter((order) => order.id !== action.payload)
+            };
+        case actionTypes.DELETE_ORDERS_FAILURE:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload
+            };
+        case actionTypes.SHIP_ORDERS_REQUEST:
+            return {
+                ...state,
+                loading: true,
+                error: null,
+            };
+        case actionTypes.SHIP_ORDERS_SUCCESS:
+            return {
+                ...state,
+                isLoading: false,
+                shipped: action.payload
+            };
+        case actionTypes.SHIP_ORDERS_FAILURE:
+            return {
+                ...state,
+                isLoading: false,
+                error: action.payload,
+            };
+        default:
+            return state;
+    }
+        
+};
+
+export default adminOrderReducer;
